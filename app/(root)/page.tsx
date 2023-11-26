55ef9761f79a2efe8414cf0e8543b11efedd0ab0
Hareesh Polla <hareeshbabu82ns@gmail.com>
2023-11-26 06:44:27 -0700

    base app layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main>
-      <h1>Threads</h1>
+      <h1 className="text-light-1">Home</h1>
     </main>
   );
 }
